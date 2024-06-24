398590f28c155c6079dd62aa8b6bb2216db0ddb1
Hasan Atmaca <104566717+Argeolog@users.noreply.github.com>
2024-06-24 23:27:26 +0300

    Onvif Zoom Focus Eklendi.

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Onvif_Camera_Search.Zoom_Focus_Form
+            Me.MainForm = Global.Onvif_Camera_Search.Anasayfa
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
